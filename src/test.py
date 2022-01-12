8e92e21f5ba76468273369da9b06cfb6f2075a67
MortalHappiness <b07901069@ntu.edu.tw>
2022-01-12 15:28:40 +0800

    Update test

--- src/test.py
+++ src/test.py
@@ -6,6 +6,7 @@ import uuid
 from main import external_merge_sort
 from constants import PAGE_SIZE
 
+random.seed(0)
 DATA_FOLDER = "./data"
 
 
@@ -18,40 +19,47 @@ def split_nums(nums, k):
     return strs
 
 
-def write_to_txt_files(strs):
-    for i, s in enumerate(strs):
-        with open(os.path.join(DATA_FOLDER, uuid.uuid4().hex + ".txt"), "w") as fout:
+def write_to_txt_files(strs, folder):
+    for s in strs:
+        with open(os.path.join(folder, uuid.uuid4().hex + ".txt"), "w") as fout:
             fout.write(s)
+
+
+def reset_folder(folder):
+    shutil.rmtree(folder, ignore_errors=True)
+    os.mkdir(folder)
 
 
 class TestExternalMergeSort(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        random.seed(0)
+        reset_folder(DATA_FOLDER)
 
-    def cleanUp(self):
-        shutil.rmtree(DATA_FOLDER, ignore_errors=True)
-        os.mkdir(DATA_FOLDER)
+    def prepare_folder(self, memory_size, total_size):
+        sub_folder = f"memory_{memory_size}_total_{total_size}"
+        data_folder = os.path.join(DATA_FOLDER, sub_folder)
+        reset_folder(data_folder)
+        return data_folder
 
     def auto_test_util(self, memory_size, total_size):
-        nums = [random.randint(1, total_size * 2) for i in range(total_size)]
-        print(memory_size, total_size, nums[:10])
+        data_folder = self.prepare_folder(
+            memory_size=memory_size, total_size=total_size)
+        nums = [random.randint(1, total_size * 2) for _ in range(total_size)]
         strs = split_nums(nums, PAGE_SIZE)
-        write_to_txt_files(strs)
+        write_to_txt_files(strs, folder=data_folder)
         nums.sort()
         sorted_strs = split_nums(nums, PAGE_SIZE)
-        external_merge_sort(memory_size, data_folder=DATA_FOLDER)
+        external_merge_sort(memory_size, data_folder=data_folder)
         for i, s in enumerate(sorted_strs):
-            with open(os.path.join(DATA_FOLDER, f"{i+1}.txt")) as fin:
+            with open(os.path.join(data_folder, f"{i+1}.txt")) as fin:
                 res = fin.read()
                 self.assertEqual(
-                    s, res, f"Unmatched result in '{i+1}.txt'. memory_size={memory_size}, total_size={total_size}")
+                    s, res, f"Unmatched result in '{i+1}.txt'. {memory_size=}, {total_size=}")
 
     def test_different_memory_size_and_numbers(self):
         for memory_size in range(300, 1100, 100):
             for total_size in [300, 5000, 10000]:
                 with self.subTest(msg=f"subtest", memory_size=memory_size, total_size=total_size):
-                    self.cleanUp()
                     self.auto_test_util(
                         memory_size=memory_size, total_size=total_size)
 
